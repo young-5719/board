75a42334e9ce7eeda27071a9bc607b912d7af601
jinyoung <djawls111@gmail.com>
2025-04-16 23:02:19 +0900

    db 구축

---
+++ rest_api.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS rest_api;
+
+CREATE DATABASE rest_api CHAR SET utf8;
+
+DROP USER IF EXISTS 'dev'@'localhost';
+CREATE USER 'dev'@'localhost' IDENTIFIED BY '1234';
+
+GRANT ALL PRIVILEGES ON rest_api.* TO 'dev'@'localhost';
+
+FLUSH PRIVILEGES;
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+CREATE SCHEMA IF NOT EXISTS `restapi` DEFAULT CHARACTER SET utf8 ;
+USE `restapi` ;
+
+CREATE TABLE IF NOT EXISTS `restapi`.`Post` (
+                                                `id` INT NOT NULL AUTO_INCREMENT,
+                                                `title` VARCHAR(100) NULL,
+                                                `content` VARCHAR(1000) NULL,
+                                                PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
